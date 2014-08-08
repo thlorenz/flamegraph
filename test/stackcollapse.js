b23cc0e954b8c99c69f819c7062c68a9c3c0911c
Thorsten Lorenz <thlorenz@gmx.de>
2014-08-08 16:41:02 -0400

    tests running properly in the browser

--- test/stackcollapse.js
+++ test/stackcollapse.js
@@ -4,35 +4,34 @@
 var test = require('tape')
   , path = require('path')
   , fs = require('fs')
+  , applyTransform = require('apply-transform')
 
 var collapseInstruments = require('../lib/stackcollapse-instruments')
 
 var fixtures = path.join(__dirname, 'fixtures') 
 var results = path.join(__dirname, 'results') 
 
-function run(collapse, name, inFilename) {
+function run(collapse, name, csv, folded) {
   test('\n' + name, function (t) {
-    var foldedFilename = inFilename.slice(0, -path.extname(inFilename).length) + '.folded';
-    var expectedFile = path.join(fixtures, foldedFilename);
-    var resultFile = path.join(results, foldedFilename);
-
-    fs.createReadStream(path.join(fixtures, inFilename))
-      .on('error', t.ifError)
-      .pipe(collapse())
-      .on('error', t.ifError)
-      .pipe(fs.createWriteStream(resultFile))
-      .on('finish', onend)
-
-    function onend() {
-      var expected = fs.readFileSync(expectedFile, 'utf8').split('\n')
-      var actual = fs.readFileSync(resultFile, 'utf8').split('\n')
-
-      t.equal(actual.length, expected.length, 'have same number of lines')
-      t.deepEqual(actual, expected, 'generates same folded content as original perl script')
+    applyTransform(collapse(), csv, function (err, actual) {
+      if (err) { t.fail(err); return t.end(); }
+      
+      t.equal(actual.length, folded.length, 'have same number of lines')
+      t.deepEqual(actual, folded, 'generates same folded content as original perl script')
       t.end()
-    }
+    })
   })
 }
 
-run(collapseInstruments, 'collapsing first part of csv file exported from instruments', 'instruments-part1.csv')
-run(collapseInstruments, 'collapsing entire csv file exported from instruments', 'instruments.csv')
+// need to make this statically analyzable for brfs
+var csvPart1 = fs.readFileSync(__dirname + '/fixtures/instruments-part1.csv', 'utf8')
+var foldedPart1 = fs.readFileSync(__dirname + '/fixtures/instruments-part1.folded', 'utf8')
+
+run(collapseInstruments, 'collapsing first part of csv file exported from instruments', csvPart1, foldedPart1)
+
+// slow due to apply transform spitting out each char separately, so only run with travis
+if (process.env.TRAVIS) {
+  var csv = fs.readFileSync(__dirname + '/fixtures/instruments.csv', 'utf8')
+  var folded = fs.readFileSync(__dirname + '/fixtures/instruments.folded', 'utf8')
+  run(collapseInstruments, 'collapsing entire csv file exported from instruments', csv, folded)
+}

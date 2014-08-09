4fbf5b87b991372a506f9933b428dd4737932ec9
Thorsten Lorenz <thlorenz@gmx.de>
2014-08-09 13:16:32 -0400

    reorganizing and stack collapse and flamegraph from array API

--- test/stackcollapse.js
+++ test/stackcollapse.js
@@ -6,14 +6,18 @@ var test = require('tape')
   , fs = require('fs')
   , applyTransform = require('apply-transform')
 
-var collapseInstruments = require('../lib/stackcollapse-instruments')
+function nonEmpty(line) {
+  return line && line.length;
+}
+
+var collapse = require('../lib/stackcollapse')
 
 var fixtures = path.join(__dirname, 'fixtures') 
 var results = path.join(__dirname, 'results') 
 
-function run(collapse, name, csv, folded) {
+function run(type, name, csv, folded) {
   test('\n' + name, function (t) {
-    applyTransform(collapse(), csv, function (err, actual) {
+    applyTransform(collapse(type), csv, function (err, actual) {
       if (err) { t.fail(err); return t.end(); }
       
       t.equal(actual.length, folded.length, 'have same number of lines')
@@ -23,15 +27,26 @@ function run(collapse, name, csv, folded) {
   })
 }
 
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
 // need to make this statically analyzable for brfs
 var csvPart1 = fs.readFileSync(__dirname + '/fixtures/instruments-part1.csv', 'utf8')
 var foldedPart1 = fs.readFileSync(__dirname + '/fixtures/instruments-part1.folded', 'utf8')
 
-run(collapseInstruments, 'collapsing first part of csv file exported from instruments', csvPart1, foldedPart1)
+run('instruments', 'collapsing first part of csv file exported from instruments', csvPart1, foldedPart1)
 
 // slow due to apply transform spitting out each char separately, so only run with travis
 if (process.env.TRAVIS) {
   var csv = fs.readFileSync(__dirname + '/fixtures/instruments.csv', 'utf8')
   var folded = fs.readFileSync(__dirname + '/fixtures/instruments.folded', 'utf8')
-  run(collapseInstruments, 'collapsing entire csv file exported from instruments', csv, folded)
+  run('instruments', 'collapsing entire csv file exported from instruments', csv, folded)
 }
+
+test('\nstack collapse from array for instruments', function (t) {
+  var arr = csvPart1.split('\n')
+  var collapsed = collapse.array('instruments', arr)
+  t.deepEqual(collapsed, foldedPart1.split('\n').filter(nonEmpty), 'collapses correclty')
+  t.end(); 
+})

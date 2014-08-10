e91dfcaddc14c3c5c5efe17a94791d053f943225
Thorsten Lorenz <thlorenz@gmx.de>
2014-08-09 23:09:11 -0400

    rendering and parsing options - currently still broken

--- web/main.js
+++ web/main.js
@@ -1,8 +1,51 @@
 'use strict';
+
 var flamegraph = require('flamegraph');
+var optsTemplate = require('./opts-template.hbs');
 var flamegraphEl = document.getElementById('flamegraph');
 var inputfileEl = document.getElementById('inputfile')
 var inputfileButtonEl = document.getElementById('inputfile-button')
+var optionsEl = document.getElementById('options');
+
+function renderOptions() {
+  var opts = flamegraph.defaultOpts
+    , meta = flamegraph.defaultOptsMeta;
+
+  var context = Object.keys(meta)
+    .reduce(function (acc, k) {
+      return acc.concat({
+          name         : k
+        , value        : opts[k]
+        , type         : meta[k].type
+        , description : meta[k].description
+      });
+    }, []);
+  var html = optsTemplate(context);
+  optionsEl.innerHTML = html;
+
+  // Need to set value in JS since it's not picked up when set in html that is added to DOM afterwards
+  Object.keys(meta)
+    .forEach(function (k) {
+      var val = opts[k];
+      var el = document.getElementById(k);
+      el.value = val;
+    });
+}
+
+function getOptions() {
+  var meta = flamegraph.defaultOptsMeta;
+
+  return Object.keys(meta)
+    .reduce(function (acc, k) {
+      var el = document.getElementById(k);
+      var val = el.value;
+      if (meta[k].type === 'number') {
+        val = val.length ? parseInt(val, 10) : Infinity;
+      }
+      acc[k] = val;
+      return acc;
+    }, {});
+}
 
 function hookHoverMethods() {
   var details = document.getElementById("details").firstChild;
@@ -27,9 +70,10 @@ function onFile(e) {
   if (!file) return;
   readFile(file, function (e) {
     var arr = e.target.result.split('\n');
+    var opts = getOptions();
     var svg;
     try {
-      svg = flamegraph.fromArray(arr);
+      svg = flamegraph.fromArray(arr, opts);
      flamegraphEl.innerHTML= svg;
      hookHoverMethods();
     } catch (err) {
@@ -42,3 +86,5 @@ inputfileEl.addEventListener('change', onFile);
 inputfileButtonEl.onclick = function () {
   inputfileEl.click();
 }
+
+renderOptions();
